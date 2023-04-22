bcbe639600c8300b1896bb8bec15d1eeaf0fdecd
aidian3k <adrian.nowosielski1@gmail.com>
2023-04-22 10:31:06 +0200

    Initial project files

---
+++ countries-app-backend/src/main/java/aidian3k/dev/countriesappbackend/CountriesAppBackendApplication.java
@@ -0,0 +1,13 @@
+package aidian3k.dev.countriesappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesAppBackendApplication.class, args);
+    }
+
+}
---
+++ countries-app-backend/src/test/java/aidian3k/dev/countriesappbackend/CountriesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package aidian3k.dev.countriesappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
